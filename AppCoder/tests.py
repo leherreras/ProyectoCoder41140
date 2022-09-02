c97b4e010225c43ff9e28dcf52d6d4e78e4f07e1
Luis Carlos Herrera <leherreras@gmail.com>
2022-09-01 22:02:22 -0300

    test

--- AppCoder/tests.py
+++ AppCoder/tests.py
@@ -10,7 +10,6 @@ class CursoTestCase(TestCase):
         Curso.objects.create(nombre="Python2", camada=2)
 
     def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
         p1 = Curso.objects.get(camada=1)
         p2 = Curso.objects.get(camada=2)
         self.assertEqual(p1.nombre, 'Python1')
@@ -19,18 +18,11 @@ class CursoTestCase(TestCase):
 
 class ViewTests(TestCase):
     def test_no_questions(self):
-        """
-        If no questions exist, an appropriate message is displayed.
-        """
         response = self.client.get(reverse('AppCoderInicio'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No hay nada para mostrar")
 
     def test_past_question(self):
-        """
-        Questions with a pub_date in the past are displayed on the
-        index page.
-        """
         curso = Curso.objects.create(nombre="Python1", camada=1)
         response = self.client.get(reverse('AppCoderCursoLeer'))
         self.assertEqual(response.status_code, 200)
